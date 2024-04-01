43ae6f7eb636db99bddcadac6cd96087ee5ab118
Aubin Olivrie <aubin.olivrie@smice.com>
2024-04-01 10:20:47 +0200

    Update

---
+++ cmd.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add all changes to the staging area
+git add .
+
+# Commit the changes with the message "Update"
+git commit -m "Update"
+
+# Push the commit to the remote repository
+git push
